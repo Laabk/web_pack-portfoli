dab1bf05196846e2f4dd6eace9829f96539f1cbb
laabk <laabkdavid@gmail.com>
2024-12-11 12:48:05 +0000

    latest blog update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://my-persna-blog-david-laab-garias-projects.vercel.app/"
+                href="https://my-persna-blog-7q.vercel.app"
                 target="_blank"
                 rel="noreferrer"
               >
